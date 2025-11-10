426db174b0226eab4c95d26f404ab56fb6051d72
Lasse-94 <laab5@student.eadania.dk>
2025-11-10 11:30:07 +0100

    superstatsdata kode

--- superstatsdata.R
+++ superstatsdata.R
@@ -1,2 +1,31 @@
 pacman::p_load(tidyverse, ggthemes, ggridges,janitor, vroom, arrow, readxl, writexl,
-              stringi, hms, tidymodels, MASS, tseries, ggfortify, polite, tufte, rvest)
+               stringi, hms, tidymodels, MASS, tseries, ggfortify, polite, tufte, rvest)
+
+min_url <- "https://superstats.dk/program?season="
+sæson_url <- "2026"
+
+for (year in 2003:2026) {
+  sæson_url <- as.character(year)
+  full_url <- paste0(min_url, sæson_url)
+}
+
+#bjarnes kode
+tables01 <- min_url %>% 
+  read_html(encoding = "UTF-8") %>% 
+  html_nodes("table") 
+
+length(tables01)
+
+tables01a <- html_table(tables01[[33]]) 
+
+html_table(tables01, convert = FALSE)
+
+?html_table
+
+
+
+
+
+
+
+
